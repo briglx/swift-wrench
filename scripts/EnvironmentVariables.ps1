157ac05650b5e531253af8cac40aa64c7310667c
Brig Lamoreaux <briglx@users.noreply.github.com>
2016-07-21 12:43:47 -0700

    Delete EnvironmentVariables.ps1

--- scripts/EnvironmentVariables.ps1
+++
@@ -1,7 +0,0 @@
-﻿# Storage Info
-
-[Environment]::SetEnvironmentVariable("ClassicStorageName", "bxdiagnostic", "User")
-[Environment]::SetEnvironmentVariable("ClassicStorageKey", "u1GM0hq+DhUq1CiGuDMT77B6xiNjaz3nmhHMcRmCqJp2yvd0ZTE2iOgnx03gdYhzgKnDkTRETvqB2pZth/T7rA==", "User")
-
-[Environment]::SetEnvironmentVariable("RmStorageName", "diagtest6744", "User")
-[Environment]::SetEnvironmentVariable("RmStorageKey", "bzju6LaboFuuGQX99JAy2/38rkFcDXiJX07DlBJ21/2lEvutoxp8wWsKaYkkQvHgBD0/rkY2x2rwQ6s5wsNm+w==", "User")
